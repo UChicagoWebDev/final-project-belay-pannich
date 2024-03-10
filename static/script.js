799cc59985a4ffe4522bfd2122f487bac11b2b67
pannich <pan.nichada@gmail.com>
2024-03-10 21:55:28 +0700

    some clean up

--- static/script.js
+++
@@ -1,5 +0,0 @@
-function showThread(threadContent) {
-  const threadMessages = document.getElementById('threadMessages');
-  threadMessages.textContent = threadContent; // Replace with actual thread content
-}
-
